e159e6b200efb42867ec7dc62ca696cd2e06648d
loganwong52 <loganwong52@gmail.com>
2025-07-18 04:41:51 -0700

    added code to vite.config.js to enable hot reload in Frontend

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true
+    },
     proxy: {
       '/api': 'http://localhost:8000'  // Forward /api/* to Django
     }
